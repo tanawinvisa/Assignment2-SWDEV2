84d230ff6ec66df418e728d971a373c0a6930cc3
tanawinvisa <tanawinvisa@gmail.com>
2023-08-15 17:13:51 +0700

    firstcommit

---
+++ register.js
@@ -0,0 +1,47 @@
+// register.js
+
+// You can add your JavaScript code here
+
+// Example: Alerting a message when the form is submitted
+// document.querySelector('form').addEventListener('submit', function(event) {
+//     event.preventDefault(); // Prevent form submission
+
+//     // Get the business name input value
+//     const businessName = document.querySelector('input[placeholder="Business Name"]').value;
+
+//     // Show an alert with the business name
+//     alert(`Thank you, ${businessName}! Your registration has been submitted.`);
+// });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.querySelector('form');
+    const chairInput = document.querySelector('.chair-input');
+    const nameInput = document.querySelector('input[placeholder="Name"]');
+    const bnameInput = document.querySelector('input[placeholder="Business Name"]');
+    const telInput = document.querySelector('input[placeholder="Telephone Number"]');
+
+
+    form.addEventListener('submit', function(event) {
+        const chairValue = parseInt(chairInput.value);
+        const bnameValue = bnameInput.value.trim();
+        const nameValue = nameInput.value.trim();
+        const telValue = telInput.value.trim();
+
+        if (bnameValue === '') {
+            event.preventDefault();
+            alert('Business Name cannot be empty.');
+        }else if (nameValue === '') {
+            event.preventDefault();
+            alert('Name cannot be empty.');
+        }else if (telValue === '') {
+            event.preventDefault();
+            alert('Telephone Number cannot be empty.');
+        }else if (isNaN(chairValue) || chairValue < 1 || chairValue > 10) {
+            event.preventDefault();
+            alert('Please enter a valid number of chairs (1-10).');
+        }else{
+            alert(`Thank you! Your registration has been submitted.`);
+            window.location.href = 'index.html';
+        }
+    });
+});
